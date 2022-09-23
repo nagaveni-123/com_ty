570156a9ba7c7859975714721a62ab8c311115af
unknown <shettigar.nagaveni@gmail.com>
2022-09-23 22:08:46 +0530

    Signup program DTO file

---
+++ Signup.java
@@ -0,0 +1,60 @@
+package com.ty.dto;
+
+public class Signup {
+	private String firstname;
+	private String lastname;
+	private String email;
+	private String password;
+	private long phone;
+	private String dob;
+	private String gender;
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public long getPhone() {
+		return phone;
+	}
+	public void setPhone(long phone) {
+		this.phone = phone;
+	}
+	public String getDob() {
+		return dob;
+	}
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	@Override
+	public String toString() {
+		return "Signup [firstname=" + firstname + ", lastname=" + lastname + ", email=" + email + ", password="
+				+ password + ", phone=" + phone + ", dob=" + dob + ", gender=" + gender + "]";
+	}
+	
+
+}
